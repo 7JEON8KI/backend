5f8179a276ba632ee84498ff0023ba5823e436a0
SoMin <thals0107@naver.com>
2024-02-10 14:58:41 +0900

    feat: 상품 리스트 조회, 상품 상세 조회 초안

--- src/main/java/com/hyundai/domain/product/dto/response/ProductResposneDTO.java
+++ src/main/java/com/hyundai/domain/product/dto/response/ProductResposneDTO.java
@@ -1,4 +1,57 @@
 package com.hyundai.domain.product.dto.response;
 
+import com.hyundai.domain.product.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResposneDTO {
+    private Long productId;
+    private String productName;
+    private String productSubName;
+    private int price;
+    private String productType;
+    private int stock;
+    private int discountRate;
+    private int amount;
+    private int calorie;
+    private String productStorage;
+    private String productDetail;
+    private String thumbnailImageUrl;
+    private String storeName;
+    private String storeTel;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public ProductResposneDTO(Product product) {
+        this.productId = product.getProductId();
+        this.productName = product.getProductName();
+        this.productSubName = product.getProductSubName();
+        this.price = product.getPrice();
+        this.productType = product.getProductType();
+        this.stock = product.getStock();
+        this.discountRate = product.getDiscountRate();
+        this.amount = product.getAmount();
+        this.calorie = product.getCalorie();
+        this.productStorage = product.getProductStorage();
+        this.productDetail = product.getProductDetail();
+        this.thumbnailImageUrl = product.getThumbnailImageUrl();
+        this.storeName = product.getStoreName();
+        this.storeTel = product.getStoreTel();
+        this.createdAt = product.getCreatedAt();
+        this.updatedAt = product.getUpdatedAt();
+    }
+
+    public static List<ProductResposneDTO> listOf(List<Product> products) {
+        return products.stream().map(ProductResposneDTO::new).collect(Collectors.toList());
+    }
 }
