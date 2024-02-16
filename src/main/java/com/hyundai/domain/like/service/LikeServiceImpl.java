0f3a45a82341622518fa21dbcddc0a4773aa01d0
skybluelion <brotherjun96@gmail.com>
2024-02-16 16:42:42 +0900

    feat: 찜 기능 좋아요/취소, 찜 목록 가져오기 구현

---
+++ src/main/java/com/hyundai/domain/like/service/LikeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.hyundai.domain.like.service;
+
+import com.hyundai.domain.like.dto.LikeProductListResponseDto;
+import com.hyundai.global.mapper.LikeMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author : 변형준
+ * @fileName : LikeServiceImpl
+ * @since : 2/15/24
+ */
+
+@Log4j
+@RequiredArgsConstructor
+@Service
+public class LikeServiceImpl implements LikeService {
+    private final LikeMapper likeMapper;
+
+    @Override
+    public Boolean saveLike(int productId, String memberId) {
+        Map<String, Object> map = Map.of("productId", productId, "memberId", memberId);
+        if(likeMapper.findLike(map).isEmpty()) {
+            likeMapper.saveLike(map);
+            return true;
+        } else {
+            likeMapper.deleteLike(map);
+            return false;
+        }
+    }
+
+    @Override
+    public List<LikeProductListResponseDto> findLikeList(String memberId) {
+        return likeMapper.findLikeList(memberId);
+    }
+
+
+}
