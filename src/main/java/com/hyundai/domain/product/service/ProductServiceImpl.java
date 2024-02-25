5ad09375c584183fc3e2dda784e8ac5e2e59fec2
SoMin <thals0107@naver.com>
2024-02-25 22:41:46 +0900

    feat: elasticsearch로 상품 검색기능 구현

--- src/main/java/com/hyundai/domain/product/service/ProductServiceImpl.java
+++ src/main/java/com/hyundai/domain/product/service/ProductServiceImpl.java
@@ -1,23 +1,31 @@
 package com.hyundai.domain.product.service;
 
 import com.hyundai.domain.login.security.CustomMemberDetails;
+import com.hyundai.domain.product.dto.request.SearchRequestDTO;
 import com.hyundai.domain.product.dto.response.ProductResponseDTO;
+import com.hyundai.domain.product.entity.Product;
+import com.hyundai.domain.product.repository.ProductSearchRepository;
 import com.hyundai.global.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
     private final ProductMapper productMapper;
+    private final ProductSearchRepository productSearchRepository;
+    private final ElasticsearchTemplate elasticsearchTemplate;
 
     @Override
     public List<ProductResponseDTO> getProducts() {
@@ -84,4 +92,45 @@ public class ProductServiceImpl implements ProductService{
         productMapper.findByTheme(params);
         return (List<ProductResponseDTO>) params.get("cursor");
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    // todo : productId -> productResponseDTO로 변경
+    public List<Long> getSearchProducts(SearchRequestDTO searchDTO) {
+        // Elasticsearch를 통한 상품 검색
+        List<Long> productIds;
+        productIds = productSearchRepository.search(searchDTO, elasticsearchTemplate);
+        log.debug("productIds : " + productIds);
+
+        return productIds;
+
+        // 상품 ID 목록으로 상품 정보 가져오기
+//        List<Product> products = productMapper.findAllByProductId(productIds);
+
+        // 상품 정보를 ProductResponseDTO로 변환하여 반환
+//        return convertToProductResponseDTOList(products);
+    }
+
+    private List<ProductResponseDTO> convertToProductResponseDTOList(List<Product> products) {
+        return products.stream()
+                .map(product -> new ProductResponseDTO(product.getProductId(),
+                        product.getProductName(),
+                        product.getProductSubName(),
+                        product.getPrice(),
+                        product.getProductType(),
+                        product.getStock(),
+                        product.getDiscountRate(),
+                        product.getProductDetail(),
+                        product.getAmount(),
+                        product.getCalorie(),
+                        product.getStorage(),
+                        product.getThumbnailImageUrl(),
+                        product.getCreatedAt(),
+                        product.getModifiedAt(),
+                        0, // isLike 필드 값 설정
+                        null)) // themeName 필드 값 설정
+                .collect(Collectors.toList());
+    }
+
+
 }
