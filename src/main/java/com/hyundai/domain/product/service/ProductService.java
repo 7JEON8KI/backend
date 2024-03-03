78228807dc629a74f1fcf15791ccae1340b12def
skybluelion <brotherjun96@gmail.com>
2024-03-03 18:21:34 +0900

    fix: 페이징 토탈 개수 / pageAmount 입력가능

--- src/main/java/com/hyundai/domain/product/service/ProductService.java
+++ src/main/java/com/hyundai/domain/product/service/ProductService.java
@@ -3,16 +3,17 @@ package com.hyundai.domain.product.service;
 import com.hyundai.domain.product.dto.request.ProductCriteria;
 import com.hyundai.domain.product.dto.request.SearchRequestDTO;
 import com.hyundai.domain.product.dto.response.ProductResponseDTO;
+import com.hyundai.domain.product.dto.response.ProductWithCountResponseDTO;
 
 import java.util.List;
 
 public interface ProductService {
-    List<ProductResponseDTO> getProducts(ProductCriteria productCriteria, String memberId);
+    ProductWithCountResponseDTO getProducts(ProductCriteria productCriteria, String memberId);
     ProductResponseDTO getProductDetail(Long productId);
 
     List<ProductResponseDTO> getSearchProducts(SearchRequestDTO searchDTO, String memberId);
 
-    List<ProductResponseDTO> getWineProducts(ProductCriteria productCriteria, String memberId);
+    ProductWithCountResponseDTO getWineProducts(ProductCriteria productCriteria, String memberId);
 
-    List<ProductResponseDTO> getThemeProducts(ProductCriteria productCriteria, String memberId);
+    ProductWithCountResponseDTO getThemeProducts(ProductCriteria productCriteria, String memberId);
 }
